72e3d12a0dee5ece41599f38d90cee1252aadc0d
OfirWasHere <ofirdoron19@gmail.com>
2023-03-21 18:38:28 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+        <a class="navbar-brand" href="#">My Vue</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <a class="nav-link" :class="{ active: activePage === index }" aria-current="page" :href="page.link.url"
+                        :title="`this link goes to ${page.link.text} page`" @click.prevent="navLinkClick(index)">{{
+                            page.link.text }}</a>
+                </li>
+            </ul>
+            <form class="d-flex ml-auto">
+                <button class="btn btn-primary" @click.prevent="changeTheme()"> Change Theme
+                </button>
+            </form>
+        </div>
+    </nav>
+
+</template>
+
+<script>
+export default {
+    props: ['pages', 'active-page', 'navLinkClick'],
+     
+    data() {
+        return {
+            theme: 'dark'
+        }
+    },
+    methods: {
+        changeTheme() {
+            let theme = 'light';
+            if (this.theme == 'light') {
+                theme = 'dark';
+            }
+            this.theme = theme;
+        }
+    }
+}
+
+</script>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>{{ page.pageTitle }}</h1>
+        <p>{{ page.content }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // Options: holds data, computed properties methods
+    props: ['page']
+}
+
+
+</script>
